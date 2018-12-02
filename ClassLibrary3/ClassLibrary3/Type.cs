834994782c8a868b314049ff8df8d6f7d04d5f7e
LEHUULY-PC\LEHUULY <lehuuly1512313@gmail.com>
2018-12-02 23:37:31 +0700

    first commit

---
+++ ClassLibrary3/ClassLibrary3/Type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class Type
+    {
+        public static Type INT = null;
+        public static Type FLOAT = null;
+        public static Type CHAR = null;
+        public static Type NVARCHAR = null;
+        public static Type VARCHAR = null;
+        public static Type NCHAR = null; 
+        public static Type BOOL = null;
+        public static Type DATE = null;
+        public static Type MONEY = null;
+    }
+}
